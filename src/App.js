c6a576e159352c45cb838b94382fd1da6eca05e5
Leigh Scheepers <86415012+bee46137@users.noreply.github.com>
2023-04-03 15:58:56 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
 
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import Home from './Components/Home';
 import Products from './Components/Products';
+import Header from './Components/Layout/Header';
+import ProductDetails from './Components/Layout/ProductDetails';
 
 const App = () => {
   return (
     <div>
-      <Route path="/Home">
-        <Home/>
-      </Route>
+      <Header />
+      <main>
+        <Switch>
+          <Route path={"/"} exact >
+            <Redirect to="/Home" />
+          </Route>
+          <Route path="/Home">
+            <Home/>
+          </Route>
 
-      <Route path="/Products">
-        <Products/>
-      </Route>
+          <Route path="/Products" exact>
+            <Products/>
+          </Route>
 
+          <Route path="/Products/:id">
+            <ProductDetails />
+          </Route>
+        </Switch>
+      </main>
+      
     </div>
   );
-}
+};
 
 export default App;
--- src/Components/Products.js
+++ src/Components/Products.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import Home from './Home'
+// import Home from './Home'
+import { Link } from 'react-router-dom';
 
 const Products = () => {
   return (
     <div>
         <h1>Product List</h1>
+        <ul>
+            <li><Link to="/products/01">Product 1</Link></li>
+            <li><Link to="/products/02">Product 2</Link></li>
+            <li><Link to="/products/03">Product 3</Link></li>
+            <li><Link to="/products/04">Product 4</Link></li>
+        </ul>
     </div>
   )
 }
 
-export default Home;
+export default Products;
